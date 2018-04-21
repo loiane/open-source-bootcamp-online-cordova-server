467ef8f816381d21a98207159abff23366f01caf
loiane <loianeg@gmail.com>
2018-04-21 18:27:53 -0400

    add cosmos db

--- mongo.js
+++ mongo.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose');
  */
 mongoose.Promise = global.Promise;
 
-const mongoUri = `mongodb://localhost:27017/contacts`;
+const mongoUri = 'mongodb://ossbootcamp:mX1b5hO5wMHvEzAdN44AnQxrgeYb47YfrddYNrCCzoFZLlXAEdwjr214esawtCEYIyN9fMkXJYJZ4bCAYBDWPg%3D%3D@ossbootcamp.documents.azure.com:10255/?ssl=true';
+
+// const mongoUri = `mongodb://localhost:27017/contacts`;
 
 function connect() {
  mongoose.set('debug', true);
